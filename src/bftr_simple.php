60547b1b8283d5204109bf7d4b3a5d1050713d3b
chainaccelerator <nicolas.cantu@chainaccelerator.com>
2019-03-05 21:04:40 +0100

    init

--- src/bftr_simple.php
+++ src/bftr_simple.php
@@ -67,7 +67,6 @@ Trait Bftr_simple
      */
     private $bftr_commit_timestamp;
 
-
     /**
      * @param string $version
      * @return bool
@@ -75,6 +74,19 @@ Trait Bftr_simple
     public function bftr_build(string $version)
     {
         $this->workflow_version = $version;
+
+        $this->build_commit_start();
+        $this->build_height_new_ready();
+        $this->build_propose();
+        $this->build_prevote();
+        $this->build_precommit();
+        $this->build_commit_end();
+        $this->build_loop();
+
+        return true;
+    }
+
+    private function build_commit_start(){
 
         $rule_height_new_prepare = new Process_rule_simple();
         $rule_height_new_prepare->build('bftr_height_new_prepare', true, false, $this->workflow_name, true, array());
@@ -87,7 +99,10 @@ Trait Bftr_simple
         $transition_commit_start->rule_list_add($rule_height_new_prepare);
         $transition_commit_start->rule_list_add($rule_broadcast_new_state_to_peers);
 
-        $this->process_workflow_init(self::$bftr_commit_target_state, $transition_commit_start);
+        return $this->process_workflow_init(self::$bftr_commit_target_state, $transition_commit_start);
+    }
+
+    private function build_height_new_ready(){
 
         $rule_height_new_wait = new Process_rule_simple();
         $rule_height_new_wait->build('bftr_height_new_wait', true, false, $this->workflow_name, true, array());
@@ -96,17 +111,26 @@ Trait Bftr_simple
         $transition_height_new_ready->build('bftr_height_new_ready', true, false);
         $transition_height_new_ready->rule_list_add($transition_height_new_ready);
 
-        $this->process_workflow_transition_add($transition_height_new_ready);
+        return $this->process_workflow_transition_add($transition_height_new_ready);
+    }
+
+    private function build_propose(){
 
         $transition_propose = new Process_transition_simple();
         $transition_propose->build('bftr_propose', true, false);
 
-        $this->process_workflow_transition_add($transition_propose);
+        return $this->process_workflow_transition_add($transition_propose);
+    }
+
+    private function build_prevote(){
 
         $transition_prevote = new Process_transition_simple();
         $transition_prevote->build('bftr_prevote', true, false);
 
-        $this->process_workflow_transition_add($transition_prevote);
+        return $this->process_workflow_transition_add($transition_prevote);
+    }
+
+    private function build_precommit(){
 
         $rule_precommit_count = new Process_rule_simple();
         $rule_precommit_count->build('bftr_precommit_count', true, false, $this->workflow_name, true, array());
@@ -115,7 +139,10 @@ Trait Bftr_simple
         $transition_precommit->build('bftr_precommit', true, false);
         $transition_precommit->rule_list_add($rule_precommit_count);
 
-        $this->process_workflow_transition_add($transition_precommit);
+        return $this->process_workflow_transition_add($transition_precommit);
+    }
+
+    private function build_commit_end() {
 
         $rule_commit_wait = new Process_rule_simple();
         $rule_commit_wait->build('bftr_commit_wait', true, false, $this->workflow_name, true, array());
@@ -140,139 +167,15 @@ Trait Bftr_simple
         $transition_commit_end->rule_list_add($rule_block_broadcast);
         $transition_commit_end->rule_list_add($rule_commit_time_set);
 
-        $this->process_workflow_transition_add($transition_commit_end);
+        return $this->process_workflow_transition_add($transition_commit_end);
+    }
+
+    private function build_loop(){
 
         $transition_loop = new Process_transition_simple();
         $transition_loop->build('bftr_loop', true, false);
 
-        $this->process_workflow_transition_add($transition_loop);
-
-        return true;
-    }
-
-    /**
-     *
-     */
-    public function bftr_prepare_new_height()
-    {
-        $this->bftr_commit = $this->block_data;
-
-        return true;
-    }
-
-    /**
-     *
-     */
-    public function bftr_broadcast_new_state_to_peers()
-    {
-        $this->bftr_commit_timestamp = time();
-
-        while ($this->process_workflow_ttl_verif_and_wait(
-                $this->bftr_commit_timestamp,
-                self::$bftr_precommit_ttl,
-                self::$bftr_precommit_wait) === false) {
-
-            return $this->bftr_broadcast_new_state_to_peers();
-        }
-        return false;
-    }
-
-    /**
-     * @param int $timestamp_start
-     * @param int $commit_time
-     */
-    public function bftr_height_new_propose()
-    {
-    }
-
-    /**
-     * @param int $timestamp_start
-     */
-    public function bftr_height_new_prevote(int $timestamp_start)
-    {
-
-
-        $this->bftr_height_new_precommit($timestamp_start, $precommit_found);
-    }
-
-    /**
-     * @param int $timestamp_start
-     * @param $precommit_found
-     */
-    public function bftr_height_new_precommit(int $timestamp_start, $precommit_found)
-    {
-
-        if ($precommit_found > $precommit_found_min) {
-
-            return $this->bftr_height_new_propose($timestamp_start, $precommit_found);
-        }
-        return return bftr_height_new_propose($commit_time);
-    }
-
-    /**
-     * @return bool
-     */
-    public function bftr_commit()
-    {
-
-        $commit_found_list = $this->bftr_commit_list_get();
-
-        foreach($commit_found_list as $bftr_commit) {
-
-            if (count($commit_found_list) > self::$bftr_commit_found_min) {
-
-                $this->bftr_commit_set_commit_time();
-
-                return $this->bftr_commit_get_block();
-            }
-            while ($this->btfr_ttl_verif_and_wait($bftr_commit->timestamp, self::$bftr_commit_ttl, self::$bftr_commit_wait) === true) {
-
-                $this->bftr_commit();
-            }
-        }
-        return false;
-    }
-
-    /**
-     * @return array
-     */
-    public function bftr_commit_list_get()
-    {
-        // @todo
-
-        return array();
-    }
-
-    /**
-     *
-     */
-    public function bftr_commit_set_commit_time()
-    {
-
-    }
-
-    /**
-     *
-     */
-    public function bftr_commit_get_block_save()
-    {
-
-    }
-
-    /**
-     *
-     */
-    public function bftr_commit_get_block_stage()
-    {
-
-    }
-
-    /**
-     *
-     */
-    public function bftr_commit_get_block_broadcast()
-    {
-
+        return $this->process_workflow_transition_add($transition_loop);
     }
 
 }
